262ee5625717699b3c01fe63c7aa02411f83debc
kohi9noor <kohinooornimes@gmail.com>
2024-12-01 18:50:59 +0530

    done: [deployement]

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "lightweight blog and portfolio.",
+  DESCRIPTION: "Portfolio",
 };
 
 export const BLOG: Metadata = {
