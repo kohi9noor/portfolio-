fad442bef973782a30ec0b85604ac41881074c07
kohi9noor <kohinooornimes@gmail.com>
2024-12-01 16:00:05 +0530

    added:[head and header and layout container for pages ]

---
+++ src/consts.ts
@@ -0,0 +1,41 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "kohinoor nimes",
+  EMAIL: "kohinoornimes@zohomail.in",
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "lightweight blog and portfolio.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "Where I have worked and what I have done.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION:
+    "A collection of my projects, with links to repositories and demos.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "github",
+    HREF: "https://github.com/kohi9noor",
+  },
+  {
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/kohinoor-nimes-41b870302/",
+  },
+];
